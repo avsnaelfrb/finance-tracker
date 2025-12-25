f78bd3e35725455dd395282df12da8e2f25dfe26
avsnaelfrb <annabhaniy@gmail.com>
2025-12-26 07:05:27 +0800

    feat: add config for drizzle

---
+++ backend/src/config/dbConfig.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise"
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined in .env file');
+  }
+  
+  const poolConnection = mysql.createPool(process.env.DATABASE_URL);
+  
+  export const db = drizzle(poolConnection);
