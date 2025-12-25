2c7ecc7a1540b5260d689f089ad68dbcb2b96603
avsnaelfrb <annabhaniy@gmail.com>
2025-12-26 04:21:33 +0800

    docs: use index route in app endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import  express, { type Application } from "express";
 import errorHandler from "./middleware/errorMiddleware.js";
+import indexRoute from './routes/indexRoute.js'
 
 const app: Application = express()
 
@@ -7,5 +8,6 @@ app.use(express.json())
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(errorHandler)
+app.use('/api', indexRoute)
 
 export default app
