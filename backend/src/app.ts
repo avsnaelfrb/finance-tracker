2569ccee5c9667515c4f4ee3d8fb592850a8ad6b
avsnaelfrb <annabhaniy@gmail.com>
2025-12-26 01:46:13 +0800

    docs: use error global middleware in app endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,11 @@
 import  express, { type Application } from "express";
+import errorHandler from "./middleware/errorMiddleware.js";
 
 const app: Application = express()
 
 app.use(express.json())
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(errorHandler)
 
 export default app
