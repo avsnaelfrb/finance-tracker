0996dc1a336962c8fc0701f3d94d4fc9aebd7e1f
avsnaelfrb <annabhaniy@gmail.com>
2025-12-26 07:05:22 +0800

    feat: add config for drizzle

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,15 @@
 import 'dotenv/config'
 import app from './app.js'
+import { db } from './config/dbConfig.js'; // Gunakan ekstensi .js karena kita pakai NodeNext
+import { users } from './models/schema.js';
 
 const port = Number(process.env.PORT_BE) || 5000
 
 app.listen(port, () => {
     console.log(`Server run on port http://localhost:${port}`);
 })
+
+
+console.log("Mencoba cek koneksi database...");
+const allUsers = await db.select().from(users);
+console.log("Koneksi aman! Data user:", allUsers);
