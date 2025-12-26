4471ebc80e10901cbc691bffd6b54401256f4384
avsnaelfrb <annabhaniy@gmail.com>
2025-12-27 03:10:07 +0800

    feat: add wallet endpoint in index route

--- backend/src/routes/indexRoute.ts
+++ backend/src/routes/indexRoute.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import userRoute from '../routes/userRoute.js'
+import walletRoute from '../routes/walletRoute.js'
 
 const router = express.Router()
 
 router.use('/user', userRoute)
+router.use('/wallet', walletRoute)
 
 export default router
