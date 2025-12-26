2d38b61cb24fe615287a428728f88df59f20dcfb
avsnaelfrb <annabhaniy@gmail.com>
2025-12-27 03:11:00 +0800

    fix: refactor import router method in user route file

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,7 +1,5 @@
-import express from 'express'
+import router from './indexRoute.js'
 import { login, register } from '../controllers/userController.js'
-
-const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
