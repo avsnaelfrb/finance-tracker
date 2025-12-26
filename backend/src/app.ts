2075c6b63053137cc1bbd1112f4729e2de911683
avsnaelfrb <annabhaniy@gmail.com>
2025-12-26 09:19:54 +0800

    fix: refactor middleware position

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,8 @@ const app: Application = express()
 app.use(express.json())
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(errorHandler)
 app.use('/api', indexRoute)
+
+app.use(errorHandler)
 
 export default app
