904d555c7655127aa9814e0f5f421cecf251190e
avsnaelfrb <annabhaniy@gmail.com>
2025-12-28 02:19:48 +0800

    fix: rename import schema file

--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise"
-import * as schema from "../models/index.js";
+import * as schema from "../models/schema.js";
 
 if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL is not defined in .env file');
