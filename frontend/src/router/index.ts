9249049332b633327c3b0dde6cd31a38cd706002
avsnaelfrb <annabhaniy@gmail.com>
2025-12-31 07:34:08 +0800

    feat: add strict group route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,24 +5,46 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/dashboard',
+      name: 'dashboard',
       component: HomeView,
-      meta: { layout: 'DefaultLayout' }
+      meta: {
+        layout: 'DefaultLayout'
+       }
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('@/views/LoginView.vue'),
-      meta: { layout: 'AuthLayout' }
+      meta: {
+        layout: 'AuthLayout',
+        isPublic: true
+       }
     },
     {
       path: '/register',
       name: 'register',
       component: () => import('@/views/RegisterView.vue'),
-      meta: { layout: 'AuthLayout' }
+      meta: {
+        layout: 'AuthLayout',
+        isPublic: true
+       }
     }
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('token')
+
+  if (to.meta.isPublic && isAuthenticated) {
+    return next('/dashboard')
+  }
+
+  if (!to.meta.isPublic && !isAuthenticated) {
+    return next('/login')
+  }
+
+  next()
 })
 
 export default router
