50bb3de250d0bce2b6724f050980f6a082f014dd
avsnaelfrb <annabhaniy@gmail.com>
2025-12-31 04:32:01 +0800

    feat: add endpoint for get user by id

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import { login, register } from '../controllers/userController.js'
+import { getById, login, register } from '../controllers/userController.js'
+import { verifyToken } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
+router.get('/', verifyToken, getById)
 
 export default router
