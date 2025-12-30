8b29b3bdeab5b794b3d891f77e64237cbd40f5d6
avsnaelfrb <annabhaniy@gmail.com>
2025-12-31 05:45:38 +0800

    feat: user store aut in default layout

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-    import { RouterLink } from 'vue-router';
+import { RouterLink } from 'vue-router';
+import { useAuthStore } from '@/stores/authStores';
+
+const auth = useAuthStore()
+
 </script>
 
 <template>
@@ -7,10 +11,15 @@
         <nav class="bg-white shadow p-4">
             <div class="container mx-auto flex gap-4">
                 <RouterLink to="/" class="font-bold">FinTrack</RouterLink>
-                <RouterLink to="/dashboard">Dashboard</RouterLink>
+                <RouterLink to="/login">Login</RouterLink>
             </div>
+            <h1 v-if="auth.isLoggedIn">{{ auth.dataUser.name }}</h1>
         </nav>
         <main class="container mx-auto p-4">
+            <pre class="bg-black text-green-400 p-4 text-xs">
+                Isi Store: {{ auth.dataUser }}
+                Status Login: {{ auth.isLoggedIn }}
+            </pre>
             <slot/>
         </main>
     </div>
