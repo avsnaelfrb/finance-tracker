0846ee510b98a58c1d13f87bafafeafbe2989616
avsnaelfrb <annabhaniy@gmail.com>
2026-01-01 23:38:44 +0800

    feat: get all wallet endpoint

--- backend/src/routes/walletRoute.ts
+++ backend/src/routes/walletRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createWallet, getById } from "../controllers/walletController.js";
+import { createWallet, getAll, getById } from "../controllers/walletController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 const router = express.Router()
 
 router.post('/add-wallet', verifyToken, createWallet)
-router.get('/get/:id', verifyToken, getById)
+router.get('/:id', verifyToken, getById)
+router.get('/all-wallet', verifyToken, getAll)
 
 export default router
