53c0a3af696116844ba344d2e7e3cb6863c75d7d
avsnaelfrb <annabhaniy@gmail.com>
2026-01-03 03:29:32 +0800

    feat: get all categories controller

--- backend/src/controllers/categoryController.ts
+++ backend/src/controllers/categoryController.ts
@@ -1,12 +1,11 @@
 import type { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../utils/catchAsync.js";
 import type { CreateCategoryRequest } from "../types/categoryType.js";
-import { createCategoryService } from "../services/categoryService.js";
+import { createCategoryService, getAllCategoryService } from "../services/categoryService.js";
 
 export const createCategory = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const body: CreateCategoryRequest = req.body;
-    const id = req.user?.id
-    const userId =  Number(id)
+    const userId = req.user?.id!
 
     const newCategory = await createCategoryService(body, userId)
 
@@ -17,3 +16,13 @@ export const createCategory = catchAsync(async (req: Request, res: Response, nex
     })
 })
 
+export const getAllCategory = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?.id!
+
+    const allCategory = await getAllCategoryService(userId)
+    res.status(200).json({
+        status: 'success',
+        message: 'Berhasil mengambil semua kategory',
+        data: allCategory
+    })
+})
