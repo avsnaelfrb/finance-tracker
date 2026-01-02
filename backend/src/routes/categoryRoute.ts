69564f89f794200a37899ed8c712897ada8d2fb6
avsnaelfrb <annabhaniy@gmail.com>
2026-01-03 03:29:46 +0800

    feat: get all categories endpoint

--- backend/src/routes/categoryRoute.ts
+++ backend/src/routes/categoryRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { verifyToken } from '../middleware/authMiddleware.js'
-import { createCategory } from '../controllers/categoryController.js'
+import { createCategory, getAllCategory } from '../controllers/categoryController.js'
 
 const router = express.Router()
 
 router.post('/add-category', verifyToken, createCategory)
+router.get('/get-all', verifyToken, getAllCategory)
 
 export default router
