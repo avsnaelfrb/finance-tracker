199455e2d3ec1c1fcb333fb7e38e4906e4d3ff5e
avsnaelfrb <annabhaniy@gmail.com>
2026-01-04 03:52:17 +0800

    feat: new transaction controller

---
+++ backend/src/controllers/transactionController.ts
@@ -0,0 +1,17 @@
+import type { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../utils/catchAsync.js";
+import type { CreateTransactionDTO } from "../types/transactionType.js";
+import { createTransactionService } from "../services/transactionService.js";
+
+export const newTransaction = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const body: CreateTransactionDTO = req.body
+    const userId = req.user?.id!
+
+    const transaction = await createTransactionService(body, userId)
+
+    res.status(201).json({
+        status: 'success',
+        message: 'Berhasil membuat transaksi baru',
+        data: transaction
+    })
+})
